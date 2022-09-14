fa755b1b2406c8149ba40128bf4a1de0a6c4fa54
Michael Vaughan <mgvaughan66@gmail.com>
2022-09-14 09:58:09 -0400

    completing 99 Lines of Code - Part 2 lab

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-// list of 5 friends
-let friends = ['Nicole', 'Jared', 'Kevin', 'John', 'David'];
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let btn = document.querySelector('button');
+    btn.addEventListener("click", function () {
 
-
-// A brand new song's descending chrous lyrics
-for (i = 0; i < friends.length; i++) {
-    console.log(friends[i].toUpperCase() + ":")
-    for (j = 99; j > 0; j--) {
-        if (j == 1) {
-            console.log(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file")
-        } else if (j == 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file")
-        } else {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file")
+        let friends = ['Nicole', 'Jared', 'Kevin', 'John', 'David'];
+        for (i = 0; i < friends.length; i++) {
+            let div = document.createElement('div');
+            div.className = 'friend';
+            let h3 = document.createElement('h3');
+            let name = document.createTextNode(friends[i]);
+            h3.appendChild(name);
+            div.appendChild(h3);
+            document.body.appendChild(div);
+            for (j = 99; j > 0; j--) {
+                if (j == 1) {
+                    let text1 = document.createTextNode(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+                    let p1 = document.createElement('p');
+                    p1.appendChild(text1);
+                    div.appendChild(p1);
+                } else if (j == 2) {
+                    let text2 = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file")
+                    let p2 = document.createElement('p');
+                    p2.appendChild(text2);
+                    div.appendChild(p2);
+                } else {
+                    let text3 = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file")
+                    let p3 = document.createElement('p');
+                    p3.appendChild(text3);
+                    div.appendChild(p3);
+                }
+            }
         }
-    }
-}
+    })
+    let removediv = document.querySelectorAll('.friend');
+    
+    removediv.forEach(friend => {
+        friend.remove();
+    });
+
+    // let div = document.createElement('div');
+    // div.className('friend');
+    // let h3 = document.createElement('h3');
+    // let name = document.createTextNode(i);
+    // h3.appendChild(name);
+    // div.appendChild(h3)
+    // document.body.appendChild(div);
+
+})
